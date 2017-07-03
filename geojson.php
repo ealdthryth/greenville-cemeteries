aed8a80562321a90d4ecf5c70222dcde6a09a16f
Christine <greeninglibrarian@gmail.com>
2017-07-03 20:31:32 +0000

    Changed names of label on map points

--- geojson.php
+++ geojson.php
@@ -1,20 +1,16 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
-
 $googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/1bUWVZT5njhPHoMHjga20J1GtvDNEpeoUNrdIKUBQqPY/pub?output=csv";
-
 $rowCount = 0;
 $features = array();
 $error = FALSE;
 $output = array();
-
 // attempt to set the socket timeout, if it fails then echo an error
 if ( ! ini_set('default_socket_timeout', 15))
 {
   $output = array('error' => 'Unable to Change PHP Socket Timeout');
   $error = TRUE;
 } // end if, set socket timeout
-
 // if the opening the CSV file handler does not fail
 if ( !$error && (($handle = fopen($googleSpreadsheetUrl, "r")) !== FALSE) )
 {
@@ -22,17 +18,15 @@ if ( !$error && (($handle = fopen($googleSpreadsheetUrl, "r")) !== FALSE) )
   while (($csvRow = fgetcsv($handle, 10000, ",")) !== FALSE)
   {
     $rowCount++;
-
     if ($rowCount == 1) { continue; } // skip the first/header row of the CSV
-
     $features[] = array(
       'type'     => 'Feature',
       'geometry' => array(
         'type'   => 'Point',
         'coordinates' => array(
-          (float) $csvRow[0],  // latitude, casted to type float
-          (float) $csvRow[1]  // longitude, casted to type float
-       )
+          (float) $csvRow[1], // longitude, casted to type float
+          (float) $csvRow[0]  // latitude, casted to type float
+        )
       ),
       'properties' => array(
         'title' => $csvRow[2],
@@ -41,28 +35,22 @@ if ( !$error && (($handle = fopen($googleSpreadsheetUrl, "r")) !== FALSE) )
       )
     );
   } // end while, loop through CSV data
-
   fclose($handle); // close the CSV file handler
-
   $output = array(
     'type' => 'FeatureCollection',
     'features' => $features
   );
 }  // end if , read file handler opened
-
 // else, file didn't open for reading
 else
 {
   $output = array('error' => 'Problem Reading Google CSV');
 }  // end else, file open fail
-
 // convert the PHP output array to JSON "pretty" format
 $jsonOutput = json_encode($output, JSON_PRETTY_PRINT);
-
 // render JSON and no cache headers
 header('Content-type: application/json; charset=utf-8');
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Access-Control-Allow-Origin: *');
-
 print $jsonOutput;
